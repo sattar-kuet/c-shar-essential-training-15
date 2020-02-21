26ae328802e10577a82c1b2b5ca23f4d045e340c
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 03:03:04 +0600

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var now = DateTime.Now;
+            Console.WriteLine(now.ToString("dd-MM-yyyy hh:mm:ss"));
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            var tomorrow = now.AddDays(1);
+            var yester = now.AddDays(-1);
+        }
+    }
+}
